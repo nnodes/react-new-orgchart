5a9e0e0b4a7199741337984f8764b57e61f27d39
Jorge A <jearaneda@uc.cl>
2021-08-12 17:02:18 -0400

    jearaneda build

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dabeng/react-orgchart/precache-manifest.e50b70ca2eb22a81776226ee555cceb6.js"
+  "/jearaneda/react-orgchart/precache-manifest.67cad75c8a00cb3dc5ca8cc765d240e9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/dabeng/react-orgchart/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/jearaneda/react-orgchart/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
